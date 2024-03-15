8ca8253721bc72fb7e981f533b877b7c5eb607f0
Gilang2812 <100253104+Gilang2812@users.noreply.github.com>
2024-03-15 14:37:21 +0700

    config tailwind

---
+++ client/src/app/(pages)/Home/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+<header className="w-svw bg-white text-red-500 h-24 container ">
+        <h1>hello world</h1>
+        <button> hello</button>
+</header>
+    )
+  }
